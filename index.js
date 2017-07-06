e653d0633c04fa35bff99cbf66aeaa6554e7df7d
Alex Berneau <haldir.alex@gmail.com>
2017-07-06 11:59:16 +0200

    feat(cli): add questions

--- index.js
+++ index.js
@@ -2,43 +2,48 @@
 
 'use strict';
 
-const chalk       = require('chalk');
-const clear       = require('clear');
-const CLI         = require('clui');
-const Spinner     = CLI.Spinner;
-const figlet      = require('figlet');
-const inquirer    = require('inquirer');
+const chalk     = require('chalk');
+const clear     = require('clear');
+const CLI       = require('clui');
+const Spinner   = CLI.Spinner;
+const figlet    = require('figlet');
+const inquirer  = require('inquirer');
 //const Preferences = require('preferences');
 //const GitHubApi   = require('github');
-const _           = require('lodash');
-const git         = require('simple-git')();
-const touch       = require('touch');
-const fs          = require('fs');
-const files       = require('./lib/files');
-const questions   = require('./lib/questions');
+const _         = require('lodash');
+const git       = require('simple-git')();
+const touch     = require('touch');
+const fs        = require('fs');
+const files     = require('./lib/files');
+const questions = require('./lib/questions');
 
 // Clear terminal
 clear();
 
 // Init cli head
-console.log(chalk.yellow(figlet.textSync('|WASA-Builder|', { horizontalLayout: 'full' })));
-console.log(chalk.cyan("Welcome to the Wasa Builder. Let's initiliaze together your boilerplate !"));
+console.log(chalk.yellow(figlet.textSync('|WASA-Builder|', {horizontalLayout: 'full'})));
+console.log(chalk.cyan('Welcome to the Wasa Builder. Let\'s initiliaze together your boilerplate !'));
+
+let projectName;
 
 //  Check if already in git repo
 if (files.directoryExists('.git')) {
-  questions.gitDetected(function() {
-    const gitDetected = arguments[0].gitDetected;
-    if (gitDetected == "yes") {
-      console.log('yes');
-    } else {
+  questions.gitContinue(function() {
+    const gitContinue = arguments[0].gitContinue;
+    if (gitContinue == true) {
+      questions.projectName(function() {
+        projectName = arguments[0].projectName.toLowerCase();
+      });
+    } else if (gitContinue == false) {
+      console.log(chalk.red.bold('Process aborted'));
       process.exit();
     }
+  })
+} else {
+  questions.projectName(function() {
+    projectName = arguments[0].projectName.toLowerCase();
+    questions.projectQuestions(projectName, () => {
+      console.log(arguments);
+    })
   });
-};
-
-// questions.projectName(() => {
-//  const projectName = arguments[0].projectName.toLowerCase();
-//  questions.projectLocation(projectName, () => {
-//    console.log(arguments);
-//  });
-// });
+}
