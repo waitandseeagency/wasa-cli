4930156bd6d8b31f66fd410950d8305ca0748cba
Alex Berneau <haldir.alex@gmail.com>
2017-07-07 12:00:32 +0200

    style(cli): change cli colors

--- index.js
+++ index.js
@@ -3,22 +3,22 @@
 'use strict';
 
 // Packages
-const chalk     = require('chalk');
-const clear     = require('clear');
-const CLI       = require('clui');
-const Spinner   = CLI.Spinner;
-const figlet    = require('figlet');
-const inquirer  = require('inquirer');
+const chalk = require('chalk');
+const clear = require('clear');
+const CLI = require('clui');
+const Spinner = CLI.Spinner;
+const figlet = require('figlet');
+const inquirer = require('inquirer');
 //const Preferences = require('preferences');
 //const GitHubApi   = require('github');
-const _         = require('lodash');
-const git       = require('simple-git')();
-const touch     = require('touch');
-const fs        = require('fs');
-const download  = require('download-git-repo')
+const _ = require('lodash');
+const git = require('simple-git')();
+const touch = require('touch');
+const fs = require('fs');
+const download = require('download-git-repo')
 
 // Files imports
-const files     = require('./lib/files');
+const files = require('./lib/files');
 const questions = require('./lib/questions');
 
 // Declaring global variables
@@ -30,7 +30,7 @@ let projectDirectory;
 clear();
 
 // Init cli head
-console.log(chalk.yellow(figlet.textSync('|WASA-Builder|', {horizontalLayout: 'full'})));
+console.log(chalk.yellow(figlet.textSync('|| WASA-CLI ||', {horizontalLayout: 'full'})));
 console.log(chalk.cyan('Welcome to the Wasa Builder. Let\'s initiliaze together your boilerplate !'));
 
 // Check if there is this already a git repo
@@ -49,7 +49,7 @@ const askContinue = () => {
     if (arguments[0].gitContinue) {
       askProjectName();
     } else {
-      console.log(chalk.red.bold('Process aborted, we advise you to move to a new directory.'));
+      console.log(chalk.red.bold('Process aborted,') + ' we advise you to move to a new directory.');
       process.exit();
     }
   })
@@ -59,37 +59,43 @@ const askContinue = () => {
 const askProjectName = () => {
   questions.projectName(function() {
     projectName = arguments[0].projectName.toLowerCase();
-    askProjectLocation();
+    console.log(`Your project will be named ${chalk.blue.bold(projectName)}.`);
+    askProjectDirectory();
   });
 }
 
 // Ask project location
-const askProjectLocation = () => {
+const askProjectDirectory = () => {
   questions.projectDirectory(function() {
     if (arguments[0].projectDirectory.length == 0 && files.directoryExists(projectName)) {
-      console.log('state 1');
-     // if input empty but directory already exists then reboot function
-     askProjectLocation();
-   } else if (arguments[0].projectDirectory.length == 0) {
-     console.log('state 2');
-     projectDirectory = projectName;
-   } else {
-     console.log('state 3');
-     projectDirectory = arguments[0].projectDirectory;
-    }
-    console.log(`project directory is ${projectDirectory}`);
-    if (gitDetected) {
-      // if git detected skip add repo
-      dlBoilerplate(projectName);
+      // if input empty but directory already exists then reboot function
+      console.log(`${chalk.red('>>')} There is already a directory named ${projectName}.`);
+      askProjectDirectory();
+    } else if (arguments[0].projectDirectory.length == 0) {
+      projectDirectory = projectName;
+      console.log('You project will be installed in the directory ' + chalk.blue.bold('./') + chalk.blue.bold(projectDirectory));
+      if (gitDetected) {
+        // if git detected skip add repo
+        dlBoilerplate(projectName);
+      } else {
+        askProjectGit();
+      }
     } else {
-      askProjectGit();
+      projectDirectory = arguments[0].projectDirectory;
+      console.log('You project will be installed in the directory' + chalk.blue.bold('./') + chalk.blue.bold(projectDirectory));
+      if (gitDetected) {
+        // if git detected skip add repo
+        dlBoilerplate(projectName);
+      } else {
+        askProjectGit();
+      }
     }
   })
 }
 
 // Ask project git
 const askProjectGit = () => {
-  questions.projectGit(function(){
+  questions.projectGit(function() {
     if (arguments[0].projectGit == 1) {
       askGitRepo();
     } else {
@@ -106,8 +112,9 @@ const askGitRepo = () => {
     // clone desired repository
     const status = new Spinner('Cloning repository, please wait...');
     status.start();
-    git.clone(gitRepo, projectDirectory).exec(function(){
+    git.clone(gitRepo, projectDirectory).exec(function() {
       status.stop();
+      console.log('Repository successfully cloned.');
       dlBoilerplate(projectName);
     });
   })
@@ -115,13 +122,15 @@ const askGitRepo = () => {
 
 // W&S Agency Boilerplate Download
 const dlBoilerplate = (projectName) => {
-  console.log(chalk.blue.bold('We will now download the boilerplate'));
+  console.log(chalk.blue.bold('We will now download the WASA Boilerplate.'));
   setTimeout(() => {
     const status = new Spinner('Downloading boilerplate, please wait...');
     status.start();
-    download('waitandseeagency/wasa-boilerplate', `${projectDirectory}/`, function (err) {
+    download('waitandseeagency/wasa-boilerplate', `${projectDirectory}/`, function(err) {
       status.stop();
-      console.log(err ? 'Error' : 'Success');
+      console.log(err
+        ? 'Error'
+        : 'We successfully downloaded the WASA Boilerplate, you\'re all set up :) !');
     });
   }, 2000)
 }
